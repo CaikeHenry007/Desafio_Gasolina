fc66b151d33685a85388f870cf464341a9dc9f1e
Caike <125682254+CaikeHenry007@users.noreply.github.com>
2024-03-22 15:34:00 -0300

    ok

--- GasolinaApp/App.js
+++ GasolinaApp/App.js
@@ -1,25 +1,10 @@
-import { View } from "react-native";
-
-
+import { Text, View } from "react-native";
+import estilos from "./src/styles/Stylesheet";
 
 export default function App() {
-
-
-
+  return (
+    <View style={estilos.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
-
-
-return (
-  <View style={estilos.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-
-
-
-
-
-
-
-
-  </View>
-);
-
